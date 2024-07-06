fa616a6d1ce1ae4a2a23a3da9c5534ea66d386d2
arefinthecybersec <86359782+arefinthecybersec@users.noreply.github.com>
2024-07-06 13:29:01 +0600

    Time: 8 ms (34.46%), Space: 15.1 MB (52.25%) - LeetHub

---
+++ 2058-concatenation-of-array/2058-concatenation-of-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        // vector<int> v;
+        // v = nums;
+        int sz = nums.size();
+        for(int i = 0; i < sz; i++) {
+            nums.push_back(nums[i]);
+        }
+
+        return nums;
+    }
+};
