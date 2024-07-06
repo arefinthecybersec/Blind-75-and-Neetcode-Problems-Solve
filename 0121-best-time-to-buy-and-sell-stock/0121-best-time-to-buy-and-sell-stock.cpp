fcc741e4c21f50e8e81c34e97ee64e278de17259
arefinthecybersec <86359782+arefinthecybersec@users.noreply.github.com>
2024-07-06 11:03:15 +0600

    Time: 75 ms (90.74%), Space: 95.8 MB (33.07%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -2,21 +2,17 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-    int n = prices.size();
-    if(n == 0) {
-        return 0;
-    }
+        int mnprice = INT_MAX, mxprofit = 0;
 
-    int min_price = INT_MAX;
-    int max_profit = 0;
+        for(int i = 0; i < prices.size(); i++) {
+            if(prices[i] < mnprice) {
+                mnprice = prices[i]; 
+            }
+            else if(prices[i] - mnprice > mxprofit) {
+                mxprofit = prices[i] - mnprice;
+            }
+        }
 
-    for(int i = 0; i < n; i++) {
-        if(prices[i] < min_price)
-            min_price = prices[i];
-        else if(prices[i] - min_price > max_profit)
-            max_profit = prices[i] - min_price;
-    }
-
-    return max_profit;
+        return mxprofit;
     }
 };
