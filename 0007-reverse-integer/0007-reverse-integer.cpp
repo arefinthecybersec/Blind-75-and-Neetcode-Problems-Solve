36c9f655bdcb08382d82f8d6530db47c8ec9cbbe
arefinthecybersec <86359782+arefinthecybersec@users.noreply.github.com>
2024-08-15 10:53:20 +0600

    Time: 0 ms (100%), Space: 9.1 MB (5.07%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     int reverse(int x) {
+        // if(x > 2147483647 || x < -2147483648)
+        //     return 0;
         string s = to_string(x);
-
-        // Reverse the string
         int m = s.length();
         for (int i = 0; i < m / 2; i++) {
-            swap(s[i], s[m - i - 1]);
+            char temp = s[i];
+            s[i] = s[m - i - 1];
+            s[m - i - 1] = temp;
         }
-
-        // If the number was negative, the '-' sign would be at the end after reversal
         if (s.back() == '-') {
-            s.pop_back();       // Remove the '-' from the end
-            s.insert(s.begin(), '-');  // Insert '-' at the beginning
+            s.insert(s.begin(), '-');
         }
-
-        // Convert the string back to an integer
-        long long p = stoll(s); // Use long long to safely check for overflow
-
-        // Check if the result is within the 32-bit signed integer range
+        long long p = stoll(s);
         if (p < -2147483648 || p > 2147483647) {
             return 0;
         }
-
         return static_cast<int>(p);
     }
 };
