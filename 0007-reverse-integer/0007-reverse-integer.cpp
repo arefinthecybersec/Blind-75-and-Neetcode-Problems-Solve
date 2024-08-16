117c56ed9d86f76e5ea456da909c2c724094b091
arefinthecybersec <86359782+arefinthecybersec@users.noreply.github.com>
2024-08-16 22:35:29 +0600

    Time: 0 ms (100%), Space: 9.3 MB (5.21%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -2,12 +2,6 @@ class Solution {
 public:
     int reverse(int x) {
         string s = to_string(x);
-        // int m = s.length();
-        // for (int i = 0; i < m / 2; i++) {
-        //     char temp = s[i];
-        //     s[i] = s[m - i - 1];
-        //     s[m - i - 1] = temp;
-        // }
 
         std::reverse(s.begin(), s.end());
         if (s.back() == '-') {
