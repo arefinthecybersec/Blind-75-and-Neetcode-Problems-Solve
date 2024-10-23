c8bdcd690c3231279248388134e79635fd14e085
arefinthecybersec <86359782+arefinthecybersec@users.noreply.github.com>
2024-10-23 21:56:54 +0600

    Time: 0 ms (100%), Space: 19.5 MB (86.53%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        // sort(nums.begin(), nums.end());
         int ck_xor = 0;
         for(int i = 0; i < nums.size(); i++) {
             ck_xor ^= nums[i];
