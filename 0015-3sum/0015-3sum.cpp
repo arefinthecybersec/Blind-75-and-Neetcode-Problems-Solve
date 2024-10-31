7e9ba0da80732c6c1db9b0486bf4a959c86ed3b5
arefinthecybersec <86359782+arefinthecybersec@users.noreply.github.com>
2024-10-31 14:37:44 +0600

    Time: 43 ms (90.33%), Space: 27.3 MB (89.24%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> triplets;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int a = nums[i];
+            int L = i+1, R = nums.size()-1;
+            int target = -a;
+            while (i + 1 < nums.size() && nums[i+1] == a) i++;
+
+            while (L < R) {
+                if (nums[L] + nums[R] > target) R--;
+                else if (nums[L] + nums[R] < target) L++;
+                else {
+                    int b = nums[L];
+                    int c = nums[R];
+                    while (L + 1 < R && nums[L + 1] == b) L++;
+                    while (L < R - 1 && nums[R - 1] == c) R--;
+                    triplets.push_back({a, b, c});
+                    
+                    L++, R--;
+                }
+            }
+        }
+        return triplets;
+    }
+};
