116037e7a709d2c683db096f00ff2c1616dcffac
arefinthecybersec <86359782+arefinthecybersec@users.noreply.github.com>
2024-11-01 10:35:57 +0600

    Time: 0 ms (100%), Space: 21.9 MB (71.71%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,8 +3,6 @@ public:
     void moveZeroes(vector<int>& nums) {
         int cnt = count(nums.begin(), nums.end(), 0);
         erase(nums, 0);
-        for (int i = 0; i < cnt; i++) {
-            nums.push_back(0);
-        }
+        for (int i = 0; i < cnt; i++) nums.push_back(0);
     }
 };
