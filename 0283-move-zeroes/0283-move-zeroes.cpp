7bb8962aa7f3294ee9c25721283016d5549d350e
arefinthecybersec <86359782+arefinthecybersec@users.noreply.github.com>
2024-11-01 10:35:22 +0600

    Time: 0 ms (100%), Space: 22 MB (26.38%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
         int cnt = count(nums.begin(), nums.end(), 0);
         erase(nums, 0);
-        
+
         for (int i = 0; i < cnt; i++) {
             nums.push_back(0);
         }
