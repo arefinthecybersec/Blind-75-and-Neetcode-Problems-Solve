7d9f4bac39f9bff5f9a82ee93adc89f3e9c7a9ff
arefinthecybersec <86359782+arefinthecybersec@users.noreply.github.com>
2024-11-12 23:52:03 +0600

    Time: 15 ms (93.77%), Space: 59.1 MB (83.6%) - LeetHub

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -0,0 +1,41 @@
+mt19937 rng(0);
+
+class Solution {
+public:
+    int partition(vector<int> &nums, int L, int R) {
+        int pvt = nums[R];
+        int i = L - 1;
+
+        for(int j = L; j < R; j++) {
+            if(nums[j] <= pvt) {
+                i++;
+                swap(nums[i], nums[j]);
+            }
+        }
+
+        swap(nums[i + 1], nums[R]);
+        return i+1;
+    }
+
+    void partial_sort(vector<int> &nums, int k) {
+        shuffle(nums.begin(), nums.end(), rng);
+        int L = 0, R = nums.size() - 1;
+        int threshold = 2 * log2(nums.size());
+
+        while(L < R && threshold--) {
+            int p = partition(nums, L, R);
+            if(p < k) L = p+1;
+            else if(p > k) R = p-1;
+            else break;
+        }
+    }
+
+    int findKthSmallest(vector<int>& nums, int k) {
+        partial_sort(nums, k);
+        return nums[k];
+    }
+
+    int findKthLargest(vector<int> &nums, int k) {
+        return findKthSmallest(nums, nums.size() - k);
+    }
+};
