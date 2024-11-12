7b5bdc486d935fbf431893b280f138c45f81c6af
arefinthecybersec <86359782+arefinthecybersec@users.noreply.github.com>
2024-11-12 22:49:40 +0600

    Time: 3 ms (41.74%), Space: 40.3 MB (27.36%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,15 +1,17 @@
+// Expected Time Complexity: o(n)
+// Expected Memory Complexity: o(n)
+// Solve Time: 1H 30M
+// Hints: Yes (approach)
+
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> prefix(n), suffix(n), result(n);
+        vector<int> prefix(n, 1), suffix(n, 1), result(n);
 
-        prefix[0] = 1;
         for (int i = 1; i < n; i++) {
             prefix[i] = prefix[i-1] * nums[i-1];
         }
-
-        suffix[n-1] = 1;
         for (int i = n-2; i >= 0; i--) {
             suffix[i] = suffix[i+1] * nums[i+1];
         }
