277b9b42b5d1d3431935a158f07752dd2a947948
arefinthecybersec <86359782+arefinthecybersec@users.noreply.github.com>
2024-12-04 19:45:17 +0600

    Time: 3 ms (71.04%), Space: 120.2 MB (83.39%) - LeetHub

---
+++ 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        auto fast = head;
+        auto slow = head;
+
+        //(n/2)th element target
+        while(fast && fast -> next) { 
+            fast = fast -> next -> next;
+            slow = slow -> next;
+        }
+
+        auto curr = head;
+        ListNode* prev = nullptr;
+
+        //1st part of (n/2) element reverse
+        while(curr != slow) {
+            auto nxt = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = nxt;
+        }
+
+        // try to implement reverse part and else part summation as a two pointer
+        ListNode* first = prev;
+        ListNode* second = slow;
+        int mx = INT_MIN;
+
+        while(first && second) {
+            int sum = first -> val + second -> val;
+            mx = max(mx, sum);
+
+            first = first -> next;
+            second = second -> next;
+        }
+        return mx;
+    }
+};
