93152d2159f4fbe08b2cf07e126e6e1736b81749
arefinthecybersec <86359782+arefinthecybersec@users.noreply.github.com>
2024-12-26 12:04:12 +0600

    Time: 7 ms (31.57%), Space: 9.8 MB (32.78%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,12 +4,7 @@ public:
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
 
-        int f = 0;
-        for(int i = 0; i < s.size(); i++) {
-            if(s[i] != t[i] || s.size() != t.size()) {
-                return false;
-            }
-        }
-        return true;
+        if(s == t) return true;
+        else return false;
     }
 };
