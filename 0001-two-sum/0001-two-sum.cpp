73366676f45a1e7c52088b41dc9918a6e1337941
arefinthecybersec <86359782+arefinthecybersec@users.noreply.github.com>
2025-01-15 23:42:48 +0600

    Time: 10 ms (47.5%), Space: 16.4 MB (7.55%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,12 +3,18 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         map<int, int> mp;
         for (int i = 0; i < nums.size(); i++) {
-            int complement = target - nums[i];
-            if (mp.find(complement) != mp.end()) {
-                return {mp[complement], i};
-            }
             mp[nums[i]] = i;
         }
+
+        for (int i = 0; i < nums.size(); i++) {
+            int f1 = nums[i];
+            int f2 = target - f1;
+
+            if(mp.find(f2) != mp.end() && mp[f2] != i) {
+                return {i, mp[f2]};
+            }
+        }
+
         return {};
     }
 };
