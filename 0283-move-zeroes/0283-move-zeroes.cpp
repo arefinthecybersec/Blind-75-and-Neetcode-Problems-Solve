ca6b5cce104191e512fce410d5427a8e0939db15
arefinthecybersec <86359782+arefinthecybersec@users.noreply.github.com>
2025-01-16 11:03:21 +0600

    Time: 87 ms (5.37%), Space: 23.8 MB (43.57%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int ck = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != 0) {
-                nums[ck] = nums[i];
-                ck++;
+        int cnt = count(nums.begin(), nums.end(), 0);
+        for(int i = nums.size()-1; i >= 0; i--) {
+            if(nums[i] == 0) {
+                nums.erase(nums.begin()+i);
             }
         }
-        for (int i = ck; i < nums.size(); i++) {
-            nums[i] = 0;
+
+        for(int i = 0; i < cnt; i++) {
+            nums.push_back(0);
+        }
+
+        for(int i = 0; i < nums.size(); i++) {
+            cout << nums[i];
         }
     }
 };
-
-//Expected Time Complexity: o(n)
-//Expected Memory Complexity: o(1)
