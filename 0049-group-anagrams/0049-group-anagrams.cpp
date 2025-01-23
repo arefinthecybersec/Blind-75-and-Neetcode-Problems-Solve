1f17c3b393063145c58793d1d6925a05433df151
arefinthecybersec <86359782+arefinthecybersec@users.noreply.github.com>
2025-01-23 15:22:48 +0600

    Time: 13 ms (82.12%), Space: 26.6 MB (27.9%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        if (strs.size() == 0) {
+        vector<string> ans;
+        ans = strs;
+        vector<pair<string, string>> result;
+
+        if(strs.size() == 0) {
             return {};
         }
-        if (strs.size() == 1) {
+        if(strs.size() == 1) {
             return {{strs[0]}};
         }
 
-        vector<string> ans = strs;
-        vector<pair<string, string>> result;
-
-        for (int i = 0; i < strs.size(); i++) {
+        for(int i = 0; i < strs.size(); i++) {
             sort(ans[i].begin(), ans[i].end());
             result.push_back({ans[i], strs[i]});
         }
+
         sort(result.begin(), result.end());
-
         vector<vector<string>> put;
-        vector<string> group;
+        vector<string> print;
 
-        for (int i = 0; i < result.size(); i++) {
-            if (i > 0 && result[i].first != result[i - 1].first) {
-                put.push_back(group);
-                group.clear();
+        for(int i = result.size() - 1; i >= 1; i--) {
+            if(result[i].first == result[i - 1].first) {
+                print.push_back(result[i].second);
             }
-            group.push_back(result[i].second);
+            else {
+                print.push_back(result[i].second);
+                put.push_back(print);
+                print.clear();
+            }
         }
-        if (!group.empty()) {
-            put.push_back(group);
-        }
+        print.push_back(result[0].second);
+        put.push_back(print);
+
         return put;
     }
 };
