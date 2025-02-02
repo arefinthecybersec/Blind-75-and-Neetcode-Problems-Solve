f248460b00a5db93d7cdc58715b8b246916dce65
arefinthecybersec <86359782+arefinthecybersec@users.noreply.github.com>
2025-02-02 12:24:31 +0600

    Time: 2 ms (38.9%), Space: 8.5 MB (82.42%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int reverse(int x) {
-        string s = to_string(x);
+        int sum = 0;
+        while(x != 0) {
+            int val = x % 10;
+            if(sum < -2147483648 || sum > 2147483647) return 0;
 
-        std::reverse(s.begin(), s.end());
-        if (s.back() == '-') {
-            s.insert(s.begin(), '-');
+            if(sum < -2147483648 / 10 || (sum == -2147483648 / 10 && val > 8)) return 0;
+            if(sum > 2147483647 / 10 || (sum == 2147483647 / 10 && val > 7)) return 0;
+
+            sum = (sum * 10) + val;
+            x /= 10;
         }
-        long long p = stoll(s);
-        if (p < -2147483648 || p > 2147483647) {
-            return 0;
-        }
-        return static_cast<int>(p);
+        return sum;
     }
 };
