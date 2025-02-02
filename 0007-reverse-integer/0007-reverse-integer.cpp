8cd267f0564b192965fc5b089a05ff89de23380c
arefinthecybersec <86359782+arefinthecybersec@users.noreply.github.com>
2025-02-02 12:43:42 +0600

    Time: 0 ms (100%), Space: 8.5 MB (82.42%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -5,8 +5,7 @@ public:
         while(x != 0) {
             int val = x % 10;
 
-            if(sum < -2147483648 / 10 || (sum == -2147483648 / 10 && val > 8)) return 0;
-            if(sum > 2147483647 / 10 || (sum == 2147483647 / 10 && val > 7)) return 0;
+            if(sum < INT_MIN / 10 || sum > INT_MAX / 10) return 0;
 
             sum = (sum * 10) + val;
             x /= 10;
