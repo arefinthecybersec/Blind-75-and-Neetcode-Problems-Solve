32c9cf8f69d0bb22c2cb866fa33309570d7aef3d
arefinthecybersec <86359782+arefinthecybersec@users.noreply.github.com>
2025-02-06 19:36:06 +0600

    Time: 0 ms (100%), Space: 8.7 MB (44.74%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x, n);
+        if(n == 0) return 1;
+
+        else if(n % 2 == 0) {
+            double ans = (pow(x, n/2)) * (pow(x, n/2));
+            return ans;
+        }
+
+        else if(n % 2 != 0) {
+            double ans = x * (pow(x, (n-1)/2)) * (pow(x, (n-1)/2));
+            return ans;
+        }
+
+        else {
+            double ans = pow(x, -n);
+            return (1/ans);
+        }
     }
 };
