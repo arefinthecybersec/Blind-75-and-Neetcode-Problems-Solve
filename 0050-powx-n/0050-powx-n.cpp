fd732fa2cc39885ac8aaa0d08e31eda6f52dc152
arefinthecybersec <86359782+arefinthecybersec@users.noreply.github.com>
2025-02-13 11:50:19 +0600

    Time: 0 ms (100%), Space: 8.7 MB (44.81%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    double recursiveApproach(double x, long long n) {
+    double myPow(double x, int n) {
         if(x == 0) return 0;
-        if(n == 0) return 1;
 
-        double res;
-        res = recursiveApproach(x, n/2);
-
-        if(n & 1) {
-            return x * res * res;
+        long long N = n;
+        if(N < 0) {
+            x = 1/x;
+            N = -N;
         }
-        else return res * res;
-    }
 
-    double myPow(double x, long long n) {
-        double ans = recursiveApproach(x, abs(n));
-
-        if(n < 0) {
-            ans = 1/ans;
+        double ans = 1;
+        while(N > 0) {
+            if(N & 1) {
+                ans *= x;
+            }
+            x *= x;
+            N /= 2;
         }
         return ans;
     }
