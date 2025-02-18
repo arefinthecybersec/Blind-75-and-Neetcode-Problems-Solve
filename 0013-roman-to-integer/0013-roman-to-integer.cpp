194c6a8b08c463793205f37cba0850a53b89bf7d
arefinthecybersec <86359782+arefinthecybersec@users.noreply.github.com>
2025-02-18 12:08:12 +0600

    Time: 10 ms (30.3%), Space: 13.3 MB (35.3%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        map<char, int> romanVal = {
+        unordered_map<char, int> romanVal = {
         {'I', 1},
         {'V', 5},
         {'X', 10},
         {'L', 50},
         {'C', 100},
         {'D', 500},
-        {'M', 1000},
+        {'M', 1000}
         };
 
         int sum = 0;
