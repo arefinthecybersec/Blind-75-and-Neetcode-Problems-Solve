b9f506b16d9e22ca58e11eeab4605b8b80434479
arefinthecybersec <86359782+arefinthecybersec@users.noreply.github.com>
2025-02-18 11:59:23 +0600

    Time: 13 ms (14.8%), Space: 13.6 MB (6.45%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        map<char, int> romanVal = {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000},
+        };
+
+        int sum = 0;
+        for(int i = 0; i < s.size(); ) {
+            if(romanVal[s[i]] < romanVal[s[i+1]]) {
+                sum += (romanVal[s[i+1]] - romanVal[s[i]]);
+                i+=2;
+            }
+            else {
+                sum += (romanVal[s[i]]);
+                i++;
+            } 
+        }
+        return sum;
+    }
+};
