dbecd0133bcce3b70111d36218df2100726b088f
arefinthecybersec <86359782+arefinthecybersec@users.noreply.github.com>
2025-02-19 11:42:19 +0600

    Time: 0 ms (100%), Space: 8.6 MB (65.67%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isSubsequence(string s, string t) {
 
-        if(s.size() == 0 && t.size() == 0) return true;
+        if(s.empty()) return true;
 
         int i = 0, j = 0;
         while(i < s.size() && j < t.size()) {
