85a8ef273d2cd1cfec26e0a70c89017a0bf6ade2
arefinthecybersec <86359782+arefinthecybersec@users.noreply.github.com>
2025-02-24 23:43:53 +0600

    Time: 1 ms (82.78%), Space: 14.7 MB (48.38%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -5,19 +5,13 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> map;
-        for(int i = 0; i < nums.size(); i++) {
-            if(map.find(nums[i]) == map.end()) {
-                map[nums[i]] = i;
-            }
-        }
 
         for(int i = 0; i < nums.size(); i++) {
             int ans = target - nums[i];
-            // map.erase(nums[i]);
-            if(map.find(ans) != map.end() && i != map[ans]) {
+            if(map.find(ans) != map.end()) {
                 return {i, map[ans]};
             }
-            // map[nums[i]] = i;
+            map[nums[i]] = i;
         }
 
         return {-1};
