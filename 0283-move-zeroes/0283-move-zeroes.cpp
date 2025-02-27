2ae27ad2a2dc67e6f9f4e547768184fa2b06a6b2
arefinthecybersec <86359782+arefinthecybersec@users.noreply.github.com>
2025-02-27 10:58:23 +0600

    Time: 3 ms (18.58%), Space: 23.6 MB (99.98%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,26 +1,18 @@
 // didn't sort & didn't coppy
-// 
+// T.C = O(n)
+// S.C = O(1)
 
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int sz = nums.size();
-        // int cntZero = count(nums.begin(), nums.end(), 0);
-        if(sz == 1) cout << nums[0];
-
         int i = 0, j = 0;
-        while(j < nums.size() && sz > 1) {
-            if(nums[i] != 0) {
-                i++, j++;
-            }
-            else if(nums[j] == 0) {
-                j++;
-            }
-            else {
+
+        while(j < nums.size()) {
+            if(nums[j] != 0) {
                 swap(nums[i], nums[j]);
                 i++;
             }
+            j++;
         }
-        for(int num: nums) cout << num << " ";
     }
 };
