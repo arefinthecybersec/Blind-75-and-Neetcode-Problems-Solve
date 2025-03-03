74f37867b10aea7ca12122005cca418fda9f60cd
arefinthecybersec <86359782+arefinthecybersec@users.noreply.github.com>
2025-03-03 12:03:59 +0600

    Time: 0 ms (100%), Space: 37.1 MB (5.64%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,6 +1,30 @@
+//T.C = O(n)
+// S.C = O(n)
+//Appr -> prefix sum
+
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        
+        int n = nums.size();
+        vector<int> leftArray(n, 0);
+        vector<int> rightArray(n, 0);
+
+        leftArray[0] = nums[0];
+        for(int i = 1; i < n; i++) {
+            leftArray[i] = nums[i] + leftArray[i-1];
+        }
+
+        rightArray[n-1] = nums[n-1];
+        for(int i = n-2; i >= 0; i--) {
+            rightArray[i] = nums[i] + rightArray[i+1];
+        }
+        // reverse(rightArray.begin(), rightArray.end());
+
+        for(int i = 0; i < n; i++) {
+            if(leftArray[i] == rightArray[i]) {
+                return i;
+            }
+        }
+        return {-1};
     }
 };
