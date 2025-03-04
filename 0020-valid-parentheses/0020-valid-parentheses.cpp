dfe5b47c4594655f79c4bedf89f4a4bb39639b78
arefinthecybersec <86359782+arefinthecybersec@users.noreply.github.com>
2025-03-04 11:55:03 +0600

    Time: 0 ms (100%), Space: 8.8 MB (35.04%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -5,20 +5,13 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-
-        // if(s.size() == 1) return false;
         
-        // st.push(s[0]);
         for(int i = 0; i < s.size(); i++) {
             if(!st.empty() && ((st.top() == '(' && s[i] == ')') || (st.top() == '{' && s[i] == '}') || (st.top() == '[' && s[i] == ']'))) {
                 st.pop();
             }
             else st.push(s[i]);
         }
-
-        if(st.empty()) {
-            return true;
-        }
-        else return false;
+        return st.empty();
     }
 };
