541dd352307441b4299bdc7eeefac7aebaf40712
arefinthecybersec <86359782+arefinthecybersec@users.noreply.github.com>
2025-03-06 13:49:04 +0600

    Time: 0 ms (100%), Space: 9 MB (81.58%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        string ans;
         int carry = 0;
+        string result;
 
-        for(int i = num1.size()-1, j = num2.size()-1; i >= 0 || j >= 0 || carry; i--,j--) {
-            int last_char1 = 0, last_char2 = 0;
+        for(int i = num1.size()-1, j = num2.size()-1; i >= 0 || j >= 0; i--, j--) {
+            int x1 = 0, x2 = 0;
+
             if(i >= 0) {
-                last_char1 = num1[i] - '0';
+                x1 = num1[i] - '0';
             }
             if(j >= 0) {
-                last_char2 = num2[j] - '0';
+                x2 = num2[j] - '0';
             }
 
-            int sum = last_char1 + last_char2 + carry;
-            carry = sum/10;
-            ans.push_back('0' + (sum%10));
+            int sum = (x1 + x2) + carry;
+            carry = 0;
+            if(sum > 9) {
+                carry = (sum / 10);
+            }
+            int ans = (sum % 10);
+            char c = ans + '0';
+            result.push_back(c);
         }
+        if(carry > 0) result.push_back(carry + '0');
+        reverse(result.begin(), result.end());
 
-        reverse(ans.begin(), ans.end());
-        return ans;
+        return result;
     }
 };
