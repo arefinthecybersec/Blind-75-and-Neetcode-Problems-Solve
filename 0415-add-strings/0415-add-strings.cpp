4e344fd26291723f78cf46535b4d3ed34fee446c
arefinthecybersec <86359782+arefinthecybersec@users.noreply.github.com>
2025-03-06 14:14:25 +0600

    Time: 0 ms (100%), Space: 9.1 MB (81.58%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        int carry = 0;
+        int carry = 0, i = num1.size()-1, j = num2.size()-1;
         string result;
 
-        for(int i = num1.size()-1, j = num2.size()-1; i >= 0 || j >= 0; i--, j--) {
+        while(i >= 0 || j >= 0 || carry) {
             int x1 = 0, x2 = 0;
 
-            if(i >= 0) {
-                x1 = num1[i] - '0';
-            }
-            if(j >= 0) {
-                x2 = num2[j] - '0';
-            }
+            if(i >= 0) x1 = num1[i] - '0';
+            if(j >= 0) x2 = num2[j] - '0';
 
             int sum = (x1 + x2) + carry;
-            carry = 0;
-            if(sum > 9) {
-                carry = (sum / 10);
-            }
-            int ans = (sum % 10);
-            char c = ans + '0';
-            result.push_back(c);
+            result.push_back((sum % 10) + '0');
+            carry = (sum / 10);
+
+            i--, j--;
         }
-        if(carry > 0) result.push_back(carry + '0');
         reverse(result.begin(), result.end());
 
         return result;
