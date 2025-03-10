571bdbb69f23bc332a121d0e4df555b714cc724c
arefinthecybersec <86359782+arefinthecybersec@users.noreply.github.com>
2025-03-10 20:12:56 +0600

    Time: 4 ms (9.04%), Space: 65.1 MB (10.26%) - LeetHub

--- 0975-range-sum-of-bst/0975-range-sum-of-bst.cpp
+++ 0975-range-sum-of-bst/0975-range-sum-of-bst.cpp
@@ -3,13 +3,7 @@ class Solution {
 public:
     int rangeSumBST(TreeNode* root, int low, int high) {
         if(root == nullptr) return 0;
-
-        int l_sum, r_sum;
         int sum = 0;
-        // if(root -> val >= low && root -> val <= high) {
-        //     l_sum += rangeSumBST(root -> left);
-        //     r_sum += rangeSumBST(root -> right);
-        // }
 
         if(root -> val >= low && root -> val <= high) sum += root -> val;
 
@@ -17,6 +11,5 @@ public:
         sum += rangeSumBST(root -> right, low, high);
 
         return sum;
-
     }
 };
